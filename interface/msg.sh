cb258ab56bb1ce7f300847130f5a810155a574d9
Millas N <millas5444@gmail.com>
2018-12-12 23:08:46 -0200

    Interface Gráfica
    Executar:
    1. cd na raiz do projeto
    2. php -S localhost:9999 -t ./interface
    3. Abra o navegador no endereco localhost:9999

---
+++ interface/msg.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+../main.out -q -t $1 -s $2 -C $3 -D $4 -A $5 2>&1 > /dev/null & ../main.out -q -t $1 2>&1 > /dev/null
---
+++ interface/protocol.php
@@ -0,0 +1,20 @@
+<?php
+	$ip_destino = $_POST['ip_destino'];
+	$porta_destino = $_POST['porta_destino'];
+	$taxa_corromper = $_POST['taxa_corromper'];
+	$taxa_perder = $_POST['taxa_perder'];
+	$taxa_atrasar = $_POST['taxa_atrasar'];
+	$dado = $_POST['dado'];
+	$size = $dado;
+
+	$type = $_POST['type'];
+	if($type == 0){
+		$size = strlen($dado);
+		system("sh msg.sh ".$size." ".$dado." ".$taxa_corromper." ".$taxa_perder." ".$taxa_atrasar);
+		
+		echo file_get_contents("mensagem");
+		echo ";".file_get_contents("mensagem_crua")."\n";
+	}else{
+		system("../main.out -q ".$padrao." -t ".$size." -f ".$dado." &"."../main.out -q -t ".$size." &");
+	}
+?>
