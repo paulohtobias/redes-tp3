f9d4b8aca8f2f775074033a8f0d1c0f7a82192b3
Paulo Tobias <paulohtobias@outlook.com>
2018-12-12 22:24:08 -0200

    parametros no script de execução

--- executar.sh
+++ executar.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+modo=$1
+mensagem=$2
+tamanho=$3
+corrompimento=$4
+descarte=$5
+atraso=$6
+
 # Abre o receptor
-./main.out -t 5753 -C 70 -A 70 -D 70 &
+#./main.out -t 5753 -C 70 -A 70 -D 70 &
+./main.out -t $tamanho -C $corrompimento -D $descarte -A $atraso &
 
 # Abre o remetente
 #./main.out -f instancias/rfc-tcp.txt -t 5753 -C 70 -A 70 -D 70
-./main.out -s a -C 70 -t 1
+./main.out -$modo $mensagem -t $tamanho -C $corrompimento -D $descarte -A $atraso &
 
 # Espera os processos terminarem
 wait
